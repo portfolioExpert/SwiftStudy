fdd23ff4583e4fdf5df1db29c5ac703a46067893
KIm yeong hyun <gus7518@naver.com>
2022-02-09 11:13:31 +0900

    Playground 개발환경 설정

---
+++ ex01.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit//ios UI 기본 라이브러리 포함
+
+//var : 변수
+//str : 이름
+//= : 대입
+//"Hello, playground" : 문자열 대입
+//세미콜론은 선택이다.
+var str = "Hello, playground";
+
+//기본 출력
+print(str)
+print("Swift Programming")//''은 지원하지 않는다.
+
+//주석문(comment): 개발자가 정보를 수록하고 실행에는 영향을 주지 않는다.
+/*
+ 여러줄
+ */
+//한 줄
